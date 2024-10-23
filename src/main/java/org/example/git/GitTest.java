55bafca12ade57b06ae653ae36d785adcfd1a96c
LiGuan-lg <130750907+LiGuan-lg@users.noreply.github.com>
2024-10-23 20:41:45 +0800

    pull commit

--- src/main/java/org/example/git/GitTest.java
+++ src/main/java/org/example/git/GitTest.java
@@ -1,14 +1,8 @@
-package org.example.git;/**
+package org.example.git;
+/**
  * @description:
  * @author: gzh
  * @create: 2024-10-23 19:22
- **/
-
-/***
- *@title GitTest
- *@description <TODO description class purpose>
- *@author li'guan
- *@create 2024/10/23 19:22
  **/
 public class GitTest {
     public static void main(String[] args) {
@@ -19,5 +13,6 @@ public class GitTest {
         System.out.println("master 冲突前commit!");
         System.out.println("hot-fix 冲突前commit!");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
