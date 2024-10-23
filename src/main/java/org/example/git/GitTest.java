55ffe3caa7f7f12c4c735813b58e6a27acda5c88
Victoria <Victoria@163.com>
2024-10-23 19:23:20 +0800

    first commit

---
+++ src/main/java/org/example/git/GitTest.java
@@ -0,0 +1,17 @@
+package org.example.git;/**
+ * @description:
+ * @author: gzh
+ * @create: 2024-10-23 19:22
+ **/
+
+/***
+ *@title GitTest
+ *@description <TODO description class purpose>
+ *@author li'guan
+ *@create 2024/10/23 19:22
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git1!");
+    }
+}
